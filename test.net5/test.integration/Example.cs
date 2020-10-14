efdd1bea5334d390d18991e948a044f47a279733
Wojciech Nagórski <wojtpl2@gmail.com>
2020-10-14 11:34:05 +0200

    Init repo

---
+++ test.net5/test.integration/Example.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace test.integration
+{
+    public class Example
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            TestServer server = new TestServer(Program.CreateHostBuilder(new string[] { })
+                .ConfigureTestServices(collection =>
+                {
+                    
+                }));
+            var client = server.CreateClient();
+            
+            Assert.NotNull(client);
+        }
+    }
+}
---
+++ test.netcore3.1/test/MvcBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using test.Validators;
+
+namespace test
+{
+    public static class MvcBuilderExtensions
+    {
+        public static IMvcBuilder SetupValidation(this IMvcBuilder builder)
+        {
+            static void RegisterValidators(FluentValidationMvcConfiguration conf)
+            {
+                Console.WriteLine("Register");
+                foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic && a.DefinedTypes.Any(t => typeof(IRequestValidator).GetTypeInfo().IsAssignableFrom(t) && !t.IsAbstract)))
+                {
+                    conf.RegisterValidatorsFromAssembly(assembly);
+                }
+            }
+
+            return builder.AddFluentValidation(RegisterValidators);
+        }
+    }
+}
